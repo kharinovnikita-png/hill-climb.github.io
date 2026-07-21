a8720b23af46c4fe52f7cb06250c435fd337f101
kharinovnikita-png <kharinovnikita@gmail.com>
2026-07-21 10:18:46 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,20 +1,10 @@
 const CACHE_NAME = 'hcr-v1';
-const ASSETS = [
-  '.',
-  'index.html',
-  'manifest.json',
-  'icon-192.png',
-  'icon-512.png'
-];
+const ASSETS = ['.', 'index.html', 'manifest.json', 'icon-192.png', 'icon-512.png'];
 
-self.addEventListener('install', (event) => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
-  );
+self.addEventListener('install', event => {
+  event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS)));
 });
 
-self.addEventListener('fetch', (event) => {
-  event.respondWith(
-    caches.match(event.request).then((response) => response || fetch(event.request))
-  );
+self.addEventListener('fetch', event => {
+  event.respondWith(caches.match(event.request).then(resp => resp || fetch(event.request)));
 });
